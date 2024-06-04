cc1c12384d18571378f61123739f0cb3d39a09be
Jay D <jdavenz8@gmail.com>
2024-06-04 18:36:17 +1200

    addded game menu, some UI changes

---
+++ game1/GameMenuScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameMenuScene.swift
+//  game1
+//
+//  Created by Jigar on 04/06/24.
+//
+
+import SwiftUI
+
+struct GameMenuScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameMenuScene()
+}
---
+++ game1/MenuScene.swift
@@ -0,0 +1,18 @@
+//
+//  MenuScene.swift
+//  game1
+//
+//  Created by Jigar on 04/06/24.
+//
+
+import SwiftUI
+
+struct MenuScene: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuScene()
+}
